2b9782aeb2c60356654d45f5f890895ccfbd7d87
sr247 <stephane@Optimus-Prime.com>
2017-01-20 12:41:16 +0100

    Td2 suite

--- TP2/TD_GL_2.c
+++ TP2/TD_GL_2.c
@@ -1,11 +1,11 @@
 ////////////////////////////////
 // 2008			      //
 // TD Animation 3D  //
-// Université Paris 11	      //  
+// Université Paris 11	      //
 // Mehdi AMMI - ammi@limsi.fr //
 ////////////////////////////////
 
-#include <GL/glut.h>    // Header pour GLUT 
+#include <GL/glut.h>    // Header pour GLUT
 #include <GL/gl.h>	// Header pour OpenGL
 #include <GL/glu.h>	// Header pour GLu
 #include <stdlib.h>     // Header  Utilitaire général
@@ -87,8 +87,7 @@ double rot_2 = 0.1;
 double rot_3 = 0.1;
 
 float angle = 0.0f;
-float move_X = 0.0f;
-float move_Z = 0.0f;
+float axis = 5.0f;
 
 /* code ASCII pour la touche escape*/
 
@@ -97,7 +96,7 @@ float move_Z = 0.0f;
 
 /* Idantifiant de la fenêtre GLUT */
 
-int window; 
+int window;
 
 
 /* Headers */
@@ -116,23 +115,23 @@ void roue();
 
 void move_camera(double speed);
 void rotate_camera(double speed);
-
+void axis_rotation(float,float,float);
 
 /* Fonction d'initialisation */
-void InitGL(int Width, int Height)	        
+void InitGL(int Width, int Height)
 {
 // Couleur d'effacement du buffer de couleur
-  glClearColor(0.3f, 0.3f, 0.4f, 0.0f);		
+  glClearColor(0.3f, 0.3f, 0.4f, 0.0f);
 
 // paramètrage du Z-buffer
-  glClearDepth(1.0);	
-  glDepthFunc(GL_LESS);	
-  glEnable(GL_DEPTH_TEST);	
+  glClearDepth(1.0);
+  glDepthFunc(GL_LESS);
+  glEnable(GL_DEPTH_TEST);
 
 // Activation de l'éclairage
   glEnable(GL_LIGHTING);
-  glEnable(GL_LIGHT0); 	
-  glEnable(GL_LIGHT1); 	
+  glEnable(GL_LIGHT0);
+  glEnable(GL_LIGHT1);
 
 // Paramètrage de l'éclairage
   glLightfv(GL_LIGHT0,GL_DIFFUSE,light_diffuse_1);
@@ -145,12 +144,12 @@ void InitGL(int Width, int Height)
   glEnable(GL_NORMALIZE);
 
 // Activation du lissage
-  glShadeModel(GL_SMOOTH);			
+  glShadeModel(GL_SMOOTH);
 
 // Projection perceptive
   glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();				
-  gluPerspective(45.0f,(GLfloat)Width/(GLfloat)Height,0.1f,1000.0f);	
+  glLoadIdentity();
+  gluPerspective(45.0f,(GLfloat)Width/(GLfloat)Height,0.1f,1000.0f);
 
   glMatrixMode(GL_MODELVIEW);
 
@@ -159,15 +158,15 @@ void InitGL(int Width, int Height)
 /* Fonction de redimensionnement de la fenêtre */
 void ReSizeGLScene(int Width, int Height)
 {
-  if (Height==0)				
+  if (Height==0)
     Height=1;
 
-  glViewport(0, 0, Width, Height);		
+  glViewport(0, 0, Width, Height);
 
 // Projection perceptive
   glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();				
-  gluPerspective(45.0f,(GLfloat)Width/(GLfloat)Height,0.1f,1000.0f);	
+  glLoadIdentity();
+  gluPerspective(45.0f,(GLfloat)Width/(GLfloat)Height,0.1f,1000.0f);
 
   glMatrixMode(GL_MODELVIEW);
 
@@ -177,9 +176,10 @@ void ReSizeGLScene(int Width, int Height)
 void DrawGLScene()
 {
 // Effacement du buffer de couleur et de profondeur
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);		
-  glLoadIdentity();		
-  
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glLoadIdentity();
+  glEnable(GL_LINE_SMOOTH);
+
 
 
 //////////////////////////////////////////////////
@@ -187,7 +187,7 @@ void DrawGLScene()
 
  gluLookAt(0.0,20.0,60.0,0.0,5.0,0.0,0.0,1.0,0.0); // 0.0,5.0,30.0
 
-// Navigation 
+// Navigation
 //...
 
 //////////////////////////////////////////////////
@@ -229,7 +229,7 @@ void DrawGLScene()
 // la base
 glTranslatef(base_x,1,base_y);
 
-glRotatef(90,0,1,0);
+glRotatef(base_rot,0,1,0);
 base();
 //..
 
@@ -252,13 +252,13 @@ glRotatef(third_x,1,0,0);
 glRotatef(third_y,0,0,1);
 third_arm();
 
-finger_1();
+glTranslatef(0,4.0,0);
 
+finger_1();
+finger_2();
 
 
 //..
-glPushMatrix();
-glPopMatrix();
 
 //////////////////////////////////////////////////
 
@@ -268,16 +268,16 @@ glPopMatrix();
 }
 
 /* Fonction de gestion du clavier */
-void keyPressed(unsigned char key, int x, int y) 
+void keyPressed(unsigned char key, int x, int y)
 {
 
-    if (key == ESCAPE) 
-    { 
+    if (key == ESCAPE)
+    {
 	/* Eteindre la fenêtre */
-	glutDestroyWindow(window); 
+	glutDestroyWindow(window);
 
 	/* Sortire du programme */
-	exit(0);               
+	exit(0);
     }
 }
 
@@ -296,29 +296,29 @@ double incr = 0.05;
 }
 
 
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
   // Pointeurs vers l'application
   glutInit(&argc, argv);
 
-  /*  Activation des buffers :   
-     Double buffer 
+  /*  Activation des buffers :
+     Double buffer
      RGBA color
      Alpha
-     Depth buffer */  
-  glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH);  
+     Depth buffer */
+  glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH);
 
   /* Création de la fenêtre */
-  glutInitWindowSize(1024, 765);  
+  glutInitWindowSize(1024, 765);
 
   /* Positionnement de la fenêtre */
-  glutInitWindowPosition(200, 200);  
+  glutInitWindowPosition(200, 200);
 
-  /* Ouverture de la fenêtre */  
-  window = glutCreateWindow("TD Animation 3D");  
+  /* Ouverture de la fenêtre */
+  window = glutCreateWindow("TD Animation 3D");
 
   /* Spécification de la fontion de dessin */
-  glutDisplayFunc(DrawGLScene);  
+  glutDisplayFunc(DrawGLScene);
 
   /* Spécification de la routine de fond */
 
@@ -336,51 +336,51 @@ int main(int argc, char **argv)
 
   /* Intitialisation des paramètres de l'affichage et de la fenêtre */
   InitGL(640, 480);
-  
-  /* Lancement de la boucle OpenGL */  
-  glutMainLoop();  
+
+  /* Lancement de la boucle OpenGL */
+  glutMainLoop();
 
   return 1;
 }
 
 
 
-void Special_key(int key, int x, int y) 
+void Special_key(int key, int x, int y)
 {
 
 // Interaction avec la base
 
-  switch (key) 
+  switch (key)
   {
-/*
-	
+
+
 	//3nd Arm
-	
-	case GLUT_KEY_RIGTH:
+
+	case GLUT_KEY_RIGHT:
 	third_x+=0.8f;
 	break;
-	
+
 	case GLUT_KEY_LEFT:
 	third_x-=0.8f;
 	break;
-	
-	case GLUT_KEY_UP:
+
+	case GLUT_KEY_DOWN:
 	third_y+=0.8f;
 	break;
-	
-	case GLUT_KEY_DOWN:
+
+	case GLUT_KEY_UP:
 	third_y-=0.8f;
 	break;
-	*/
-//	case ...:  		
+	
+//	case ...:
 //	...
-//		break; 
+//		break;
 
 //..
 
         default:
         break;
-  }    
+  }
 
 
 
@@ -391,13 +391,13 @@ void Special_key(int key, int x, int y)
 void Keyboard_key(unsigned char key, int x, int y)
 {
 
-  switch (key) 
-  {    
+  switch (key)
+  {
 
 ///////////////////////////////////////////
 // Interaction avec le bras
 
-//	case '...':  
+//	case '...':
 
 //base
 	case 's':
@@ -407,58 +407,79 @@ void Keyboard_key(unsigned char key, int x, int y)
 	case 'z':
 	base_y-=0.8f;
 	break;
-	
+
 	case 'd':
 	base_x+=0.8f;
 	break;
-	
+
 	case 'q':
 	base_x-=0.8f;
 	break;
 	
+	case 'a':
+	base_rot+=1.0f;
+	break;
+
+	case 'e':
+	base_rot-=1.0f;
+	break;
 	// 1st Arm
-	
+
 	case 'f':
-	first_x+=0.8f;
+	first_x-=0.8f;
 	break;
 
 	case 'h':
-	first_x-=0.8f;
+	first_x+=0.8f;
 	break;
-	
+
 	case 't':
-	first_y+=0.8f;
+	first_y-=0.8f;
 	break;
 
 	case 'g':
-	first_y-=0.8f;
+	first_y+=0.8f;
 	break;
-	
+
 	//2nd Arm
-	
+
 	case 'j':
-	second_x+=0.8f;
-	break;
-	
-	case 'l':
 	second_x-=0.8f;
 	break;
-	
+
+	case 'l':
+	second_x+=0.8f;
+	break;
+
 	case 'i':
+	second_y-=0.8f;
+	break;
+
+	case 'k':
 	second_y+=0.8f;
 	break;
 	
-	case 'k':
-	second_y-=0.8f;
+	case ' ':
+	first_x = first_y = second_x = second_y = third_x = third_y = 0.0f;
+	base_rot = 90.0f;
 	break;
 	
 	
+	case 'c':
+	finger_x+= 0.9f;
+	break;
 	
-	
+	case 'w':
+	finger_x-= 0.9f;
+	break;
+
+
+
+
 
 //		...
 //		...
-//		break; 
+//		break;
 
 //
 //
@@ -475,14 +496,14 @@ void Keyboard_key(unsigned char key, int x, int y)
 //////////////////////////////////////////////////
 
         case ESCAPE :
-               { 
-	       glutDestroyWindow(window); 
-	       exit(0);                   
+               {
+	       glutDestroyWindow(window);
+	       exit(0);
                }
 
-        default: 
+        default:
         break;
-  }    
+  }
 
    glutPostRedisplay();
    glutSwapBuffers();
@@ -495,8 +516,8 @@ void ground()
 
   glDisable(GL_LIGHTING);
   glColor3f(0.5,0.5,0.0);
+  glLineWidth(1.0);
 
-	
 
 	// Draw a 1x1 grid along the X and Z axis'
         float i;
@@ -536,14 +557,17 @@ void base()
 
 		glPushMatrix();
 
-		glRotatef(90.0,1.0,0.0,0.0);
+		glRotatef(90.0,1.0,0.0,0.0); // roatation autour de l'axe x : L'axe z devient y  et l'axe y devient z
 
 		GLUquadricObj *p;
 		p = gluNewQuadric();
 		gluCylinder(p, 3.0, 3.0,1.0, 30, 30);
 		gluDisk(p, 0.0,3.0, 50, 5);
+				
+		axis_rotation(0.0,0.0,axis); // Donc l'axe de la base ici est z ( mais en réalité y )
+		
 		glPopMatrix();
-        glTranslatef(0.0,0.0,0.0); 
+        glTranslatef(0.0,0.0,0.0);
 		gluSphere(p, 1.0, 30, 30);
 
         gluDeleteQuadric(p);
@@ -558,17 +582,19 @@ void first_arm()
 
 		SetMaterial(mat_specularBLUE, mat_ambientBLUE, mat_diffuseBLUE, mat_shininessBLUE);
 
+		axis_rotation(0.0, 0.0, axis);
+		axis_rotation(axis, 0.0, 0.0);
 		glTranslatef(0.0,2.0,0.0); // 1/2 hauteur du segment
 
 		glPushMatrix();
-		glScalef(1.2,4.0,1.2);	
+		glScalef(1.2,4.0,1.2);
 		glutSolidCube(1);
 		glPopMatrix();
 
-                glTranslatef(0.0,2.0,0.0);
-                glutSolidSphere(1.0,20,20);
+        glTranslatef(0.0,2.0,0.0);
+        glutSolidSphere(1.0,20,20);
 
-        glPopMatrix();
+	glPopMatrix();
 }
 
 
@@ -581,10 +607,13 @@ glPushMatrix();
 
 		SetMaterial(mat_specularBLUE, mat_ambientBLUE, mat_diffuseBLUE, mat_shininessBLUE);
 
+		axis_rotation(0.0, 0.0, axis);
+		axis_rotation(axis, 0.0, 0.0);
+		
 		glTranslatef(0.0,2.0,0.0); // 1/2 hauteur du segment
 
 		glPushMatrix();
-		glScalef(0.8,4.0,0.8);	
+		glScalef(0.8,4.0,0.8);
 		glutSolidCube(1);
 		glPopMatrix();
 
@@ -606,17 +635,20 @@ glPushMatrix();
 
 		SetMaterial(mat_specularBLUE, mat_ambientBLUE, mat_diffuseBLUE, mat_shininessBLUE);
 
+		axis_rotation(0.0, 0.0, axis);
+		axis_rotation(axis, 0.0, 0.0);
+		
 		glTranslatef(0.0,2.0,0.0); // 1/2 hauteur du segment
 
 		glPushMatrix();
-		glScalef(0.6,4.0,0.6);	
+		glScalef(0.6,4.0,0.6);
 		glutSolidCube(1);
 		glPopMatrix();
-		
-		glTranslatef(0.0,2.0,0.0);
-		glutSolidSphere(1.0,20,20);
 
-        glPopMatrix();
+                glTranslatef(0.0,2.0,0.0);
+                glutSolidSphere(1.0,20,20);
+
+glPopMatrix();
 
 }
 
@@ -625,26 +657,36 @@ glPushMatrix();
 void finger_1()
 {
 //..
+
+glPushMatrix();
+glTranslatef(0.8,0.0,0.0);
+glRotatef(finger_x,0,0,1);
+axis_rotation(0.0, 0.0, axis);
 glPushMatrix();
 
-		SetMaterial(mat_specularBLUE, mat_ambientBLUE, mat_diffuseBLUE, mat_shininessBLUE);
-
-		glTranslatef(0.0,5.0,0.5);
-		glPushMatrix();		
-		glScalef(0.2,1.0,0.2);	
-		glutSolidCube(1);				
-		glPopMatrix();			
-		
-		glPushMatrix();		
-		glTranslatef(0.0,1.0,0.0);
-		glRotatef(-32,0,0,1);
-		glScalef(0.2,1.0,0.2);	
-		glutSolidCube(1);
+	SetMaterial(mat_specularBLUE, mat_ambientBLUE, mat_diffuseBLUE, mat_shininessBLUE);
+	glTranslatef(0.0,1.0,0.0);
+	
+		glPushMatrix();	
+			glTranslatef(0.0,-1.0,0.0);
+			
 		glPopMatrix();
-                  
-
+		
+		glPushMatrix();
+			glScalef(0.2,1.0,0.2);
+			glutSolidCube(1);
+		glPopMatrix();
+                
+		glTranslatef(-0.3,1.0,0.0);
+                
+		glPushMatrix();
+			glRotatef(32,1,0,25);
+			glScalef(0.2,1.0,0.2);
+			glutSolidCube(1);                
+		glPopMatrix();
+    
 glPopMatrix();
-
+glPopMatrix();
 }
 
 
@@ -652,6 +694,35 @@ void finger_2()
 {
 //..
 
+glPushMatrix();
+glTranslatef(-0.8,0.0,0.0);
+glRotatef(-finger_x,0,0,1);
+axis_rotation(0.0, 0.0, axis);
+glPushMatrix();
+
+	SetMaterial(mat_specularBLUE, mat_ambientBLUE, mat_diffuseBLUE, mat_shininessBLUE);
+	glTranslatef(0.0,1.0,0.0);
+    
+		glPushMatrix();	
+			glTranslatef(0.0,-1.0,0.0);
+			
+		glPopMatrix();
+		
+		glPushMatrix();
+			glScalef(0.2,1.0,0.2);
+			glutSolidCube(1);
+		glPopMatrix();
+                
+		glTranslatef(0.3,1.0,0.0);
+                
+		glPushMatrix();
+			glRotatef(-32,1,0,25);
+			glScalef(0.2,1.0,0.2);
+			glutSolidCube(1);                
+		glPopMatrix();
+    
+glPopMatrix();
+glPopMatrix();
 }
 
 
@@ -684,27 +755,19 @@ void rotate_camera(double speed)
 
 }
 
-
-/*
- * 
- * 
- * void first_arm()
-{
-        glPushMatrix();
-
-                SetMaterial(mat_specularBLUE, mat_ambientBLUE, mat_diffuseBLUE, mat_shininessBLUE);
-
-                glTranslatef(0.0,2.0,0.0); // 1/2 hauteur du segment
-
-                glPushMatrix();
-                glScalef(1.2,4.0,1.2);
-                glutSolidCube(1);
-                glPopMatrix();
-
-                glTranslatef(0.0,2.0,0.0);
-                glutSolidSphere(1.0,20,20);
-
-        glPopMatrix();
+void axis_rotation(float axe_x,float axe_y, float axe_z){
+	
+	glPushMatrix();	
+	glLineWidth(2.0);	
+	
+	glDisable(GL_LIGHTING);
+	glColor3f(0.0,1.0,0.0);
+	
+	glBegin(GL_LINES);
+	glVertex3f(-axe_x, -axe_y, -axe_z);
+	glVertex3f(axe_x, axe_y, axe_z);
+	glEnd();
+	glPopMatrix();
+	
+	glEnable(GL_LIGHTING);
 }
-
-*/
